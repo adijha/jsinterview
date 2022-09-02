28ae21ad1df4a47684319775969b1327230c5ee3
Aditya Jha <mail@adijha.com>
2022-09-03 03:13:12 +0530

    tailwind-integrated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
 	return (
 		<div className="min-h-screen flex justify-center items-center border-2">
-			<h1 className="text-3xl font-bold underline border-2">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline border-2">TS+ Tailwind+ Vite working</h1>
 		</div>
 	)
 }
