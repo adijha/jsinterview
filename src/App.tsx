c81266683bb159c3a22c72613953166715f0e36b
Aditya Jha <mail@adijha.com>
2022-09-03 03:09:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+	return (
+		<div className="min-h-screen flex justify-center items-center border-2">
+			<h1 className="text-3xl font-bold underline border-2">Hello world!</h1>
+		</div>
+	)
+}
+
+export default App
