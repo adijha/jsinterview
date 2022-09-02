0e4aa4522fe8c82bff16d40aba77b05e1595610a
Aditya Jha <mail@adijha.com>
2022-09-03 04:11:34 +0530

    layout-ready

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,32 @@ import { Console } from './components/console'
 
 export default function App() {
 	const [input, setInput] = useState('')
+	const defaultCode = `console.log("abba harmonium bajate hai")`
+	
+	function runCode(params: type) {
+		var fnparams = [1, 2, 3]
+		console.log('first')
+		const fn = window[defaultCode]
+		if (typeof fn === 'function') fn.apply(null, fnparams)
+	}
 
 	return (
 		<div className="App">
-			<Editor
-				placeHolder="Type your code here..."
-				onChange={(e) => setInput(e.target.value)}
-			/>
-			<Console></Console>
+			<div className="container">
+				<Editor
+					placeHolder="Type your code here..."
+					onChange={(e) => setInput(e.target.value)}
+					defaultValue={defaultCode}
+				/>
+				<Console></Console>
+			</div>
+			<button
+				onClick={runCode}
+				type="button"
+				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+			>
+				Run Javascript using browser v8
+			</button>
 		</div>
 	)
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,12 @@
 import './Editor.css'
 
-export const Editor = ({ placeHolder, onChange, onKeyDown }) => {
+export const Editor = ({ placeHolder, onChange, onKeyDown, defaultValue }) => {
 	return (
 		<textarea
 			className="editor"
 			placeholder={placeHolder}
 			onChange={onChange}
+			defaultValue={defaultValue}
 		></textarea>
 	)
 }
